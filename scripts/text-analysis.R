aa6839be878348929e23cc88fe6f679f9e4fdce8
Matt Farrow <matt@formalizedcuriosity.net>
2021-02-21 14:57:27 -0600

    drop 'bottom 10', add occupations, locations

--- scripts/text-analysis.R
+++ scripts/text-analysis.R
@@ -25,32 +25,52 @@ answers <- df_board %>%
   select(answer, uid) %>% 
   distinct()
 
+# Break out occupations
+occupations <- as_tibble(unlist(strsplit(df_players$occupation, "\n|,|, ")))
+occupations <- as_tibble(unlist(strsplit(occupations$value, " And ")))
+occupations <- as_tibble(str_remove_all(occupations$value, "An "))
+
+# Break out locations
+locations <- str_split_fixed(df_players$from, ", ", 2)
+locations <- tibble("City" = locations[,1],
+                    "State" = locations[,2])
+locations <- locations %>%
+  mutate("ST" = state.abb[match(locations$State, state.name)])
+locations$ST[is.na(locations$ST)] <- "DC"
+
 # Text Analysis -----------------------------------------------------------
 
-# Top 10 categories
+# Top 10 Categories
 categories %>% 
   count(category) %>% 
   arrange(desc(n)) %>% 
   slice_head(n = 10) %>% 
   rename(games = n)
 
-# Bottom 10 categories
-categories %>% 
-  count(category) %>% 
-  arrange(desc(n)) %>% 
-  slice_tail(n = 10) %>% 
-  rename(games = n)
-
-# Top 10 answers
+# Top 10 Answers
 answers %>% 
   count(answer) %>% 
   arrange(desc(n)) %>% 
   slice_head(n = 10) %>% 
   rename(games = n)
 
-# Bottom 10 answers
-answers %>% 
-  count(answer) %>% 
+# Top 10 Occupations
+occupations %>% 
+  count(value) %>% 
   arrange(desc(n)) %>% 
-  slice_tail(n = 10) %>% 
-  rename(games = n)
+  slice_head(n = 10) %>% 
+  rename(players = n)
+
+# Top 10 States
+locations %>% 
+  count(ST) %>% 
+  arrange(desc(n)) %>% 
+  slice_head(n = 10) %>% 
+  rename(players = n)
+
+# Top 10 City/ST Combos
+locations %>% 
+  count(City, ST) %>% 
+  arrange(desc(n)) %>% 
+  slice_head(n = 10) %>% 
+  rename(players = n)
