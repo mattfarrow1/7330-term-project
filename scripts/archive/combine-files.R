9c24c9f5faabea2cf5e8c2fbcff4c5c14ab72156
Matt Farrow <matt@formalizedcuriosity.net>
2021-02-25 14:05:21 -0600

    update data with final games

--- scripts/archive/combine-files.R
+++ scripts/archive/combine-files.R
@@ -32,56 +32,64 @@ board_1501_6000 <- read_excel("data - raw/games/board_1501_6000.xlsx")
 
 # Combine Files Together --------------------------------------------------
 
-df_airdate <- bind_rows(airdate,
+airdate <- bind_rows(airdate,
                         airdate_501_1500,
-                        airdate_1501_6000)
+                        airdate_1501_6000,
+                        airdate_6001_6948)
 
-df_board <- bind_rows(board,
+board <- bind_rows(board,
                       board_501_1500,
-                      board_1501_6000)
+                      board_1501_6000,
+                      board_6001_6948)
 
-df_doubles <- bind_rows(doubles,
+doubles <- bind_rows(doubles,
                         doubles_501_1500,
-                        doubles_1501_6000)
+                        doubles_1501_6000,
+                        doubles_6001_6948)
 
-df_players <- bind_rows(players,
+players <- bind_rows(players,
                         players_501_1500,
                         players_1501_6000)
 
-df_scores <- bind_rows(scores,
+scores <- bind_rows(scores,
                        scores_501_1500,
-                       scores_1501_6000)
+                       scores_1501_6000,
+                       scores_6001_6948)
 
-df_synopsis <- bind_rows(synopsis,
+synopsis <- bind_rows(synopsis,
                          synopsis_501_1500,
-                         synopsis_1501_6000)
+                         synopsis_1501_6000,
+                         synopsis_6001_6948)
 
 # Save Files --------------------------------------------------------------
 
-write_csv(df_airdate, here::here("data - output", "airdate_0001_6000.csv"))
-write_csv(df_board, here::here("data - output", "board_0001_6000.csv"))
-write_csv(df_doubles, here::here("data - output", "doubles_0001_6000.csv"))
-write_csv(df_scores, here::here("data - output", "scores_0001_6000.csv"))
-write_csv(df_synopsis, here::here("data - output", "synopsis_0001_6000.csv"))
+write_csv(airdate, here::here("data - output", "airdate_0001_6948.csv"))
+write_csv(board, here::here("data - output", "board_0001_6948.csv"))
+write_csv(doubles, here::here("data - output", "doubles_0001_6948.csv"))
+write_csv(scores, here::here("data - output", "scores_0001_6948.csv"))
+write_csv(synopsis, here::here("data - output", "synopsis_0001_6948.csv"))
 
 # Save RData --------------------------------------------------------------
 
+load(here::here("data - output", "game_contestants.RData"))
+load(here::here("data - output", "player_ids.RData"))
+
 # Everything
-save(df_airdate,
-     df_board,
-     df_doubles,
-     df_players,
-     df_scores,
-     df_synopsis,
+save(airdate,
+     board,
+     doubles,
+     game_contestants,
+     player_ids,
+     scores,
+     synopsis,
      file = here::here("data - output", "jeopardy.RData"))
 
 # Individual Files
-save(df_airdate, file = here::here("data - output", "airdate.RData"))
-save(df_board, file = here::here("data - output", "board.RData"))
-save(df_doubles, file = here::here("data - output", "doubles.RData"))
-save(df_players, file = here::here("data - output", "players.RData"))
-save(df_scores, file = here::here("data - output", "scores.RData"))
-save(df_synopsis, file = here::here("data - output", "synopsis.RData"))
+save(airdate, file = here::here("data - output", "airdate.RData"))
+save(board, file = here::here("data - output", "board.RData"))
+save(doubles, file = here::here("data - output", "doubles.RData"))
+save(scores, file = here::here("data - output", "scores.RData"))
+save(synopsis, file = here::here("data - output", "synopsis.RData"))
 
 # Load Data ---------------------------------------------------------------
 
