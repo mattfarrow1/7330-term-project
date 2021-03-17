fdf65bb07dd950164d97116a22ef54cb5c7e514d
Megan Ball <49882940+megball@users.noreply.github.com>
2021-03-16 21:35:06 -0500

    added some queries and a bar chart

--- scripts/query-database.R
+++ scripts/query-database.R
@@ -6,8 +6,14 @@ library(RMariaDB)
 library(DBI)
 library(dbplyr)
 
+
 # Create connection
-con <- dbConnect(RMariaDB::MariaDB(), dbname = "jeopardy")
+con <- dbConnect(RMariaDB::MariaDB(),
+                 dbname = "jeopardy",
+                 user = "root",
+                 password = "***"
+                 )
+#
 
 # Examples ----------------------------------------------------------------
 
@@ -48,8 +54,47 @@ res <- dbSendQuery(
   ORDER BY count(*) DESC;"
 )
 
+#List of everyone who won double jeopardy
+who_db <- dbSendQuery(
+  con,
+  "select board.clueid, category, clue, answer, players.playerid, players.firstname, players.lastname
+  from board
+  INNER JOIN doubles_has_scores on doubles_has_scores.clueid = board.clueid
+  INNER JOIN players on players.playerid = doubles_has_scores.playerid
+  where doublejeop = 1;"
+)
+
+#top double jeopardy contestants
+top <- dbSendQuery(
+  con,
+  "select count(board.clueid) as double_jeop_count, players.playerid, players.firstname, players.lastname
+  from board
+  INNER JOIN doubles_has_scores on doubles_has_scores.clueid = board.clueid
+  INNER JOIN players on players.playerid = doubles_has_scores.playerid
+  where doublejeop = 1
+  GROUP BY playerid
+  order by double_jeop_count desc
+  limit 10;"
+)
+
+#for each top 10 player, where did they select the double jeopardy the most?
+top10_dj <- dbSendQuery(
+  con,
+  "select count(board.clueid) as loc_count, players.playerid, players.firstname, players.lastname, location.rowcat, location.colcat
+  from board
+  INNER JOIN doubles_has_scores on doubles_has_scores.clueid = board.clueid
+  INNER JOIN location on board.clueid = location.board_clueid
+  INNER JOIN players on players.playerid = doubles_has_scores.playerid
+  where doublejeop = 1 and players.playerid in (1, 12600, 861, 12824, 9037, 10171, 11663, 8885, 10911, 8522)
+  group by playerid, rowcat, colcat
+  order by playerid;"
+)
+
 # Save results
 doubles <- dbFetch(res, n = Inf)
+who_db <- dbFetch(who_db, n = Inf)
+top <- dbFetch(top, n = Inf)
+top10_dj <- dbFetch(top10_dj, n = Inf)
 
 # Clear results
 dbClearResult(res)
@@ -101,3 +146,25 @@ doubles <- doubles %>%
 doubles %>% 
   ggplot(aes(as.character(location), instances)) +
   geom_col()
+
+# Top 10 Double Jeopardy count ----------
+
+#create new column combining first and last name
+top <- top %>%
+  mutate(name = paste(firstname,lastname))
+
+#change double_jeop_count to numeric
+top$double_jeop_count <- as.numeric(top$double_jeop_count)
+
+top %>%
+  arrange(double_jeop_count) %>%
+  ggplot(aes(x = reorder(name, double_jeop_count), y = double_jeop_count)) +
+  geom_col(fill="darkblue") +
+  coord_flip() +
+  xlab("Contestant") +
+  ylab("Career Double Jeopardy Count") +
+  theme_light() +
+  ggtitle("Who Got the Most Double Jeopardy Clues?")
+
+
+  
