aba7bcbec7d143361ba73113b9823d61310e9e3f
Megan Ball <49882940+megball@users.noreply.github.com>
2021-03-24 20:08:00 -0500

    added additional analysis. added word cloud plots for daily double clues, categories, and answers

--- scripts/mysql_database-analysis.R
+++ scripts/mysql_database-analysis.R
@@ -3,6 +3,9 @@
 
 source(here::here("scripts", "query-database.R"))
 library(scales)
+#for text analysis
+library(tm)
+library(wordcloud)
 
 # Top Categories ----------------------------------------------------------
 
@@ -104,12 +107,130 @@ top %>%
 
 ggsave(here::here("images", "top-daily-double-clues.png"), dpi = "retina")
 
-# Daily Double Wagers -----------------------------------------------------
 
 
-# Top Daily Double Categories ---------------------------------------------
+# Top Daily Double Key Words ---------------------------------------------
+#create corpus from the category, clue & answer information
+clue_corpus <- SimpleCorpus(VectorSource(daily_doubles$clue))
+category_corpus <- SimpleCorpus(VectorSource(daily_doubles$category))
+answer_corpus <- SimpleCorpus(VectorSource(daily_doubles$answer))
+
+# 1. Stripping any extra white space:
+clue_corpus <- tm_map(clue_corpus, stripWhitespace)
+category_corpus <- tm_map(category_corpus, stripWhitespace)
+answer_corpus <- tm_map(answer_corpus, stripWhitespace)
+
+# 2. Transforming everything to lowercase
+clue_corpus <- tm_map(clue_corpus, content_transformer(tolower))
+category_corpus <- tm_map(category_corpus, content_transformer(tolower))
+answer_corpus <- tm_map(answer_corpus, content_transformer(tolower))
+
+# 3. Removing numbers 
+clue_corpus <- tm_map(clue_corpus, removeNumbers)
+category_corpus <- tm_map(category_corpus, removeNumbers)
+answer_corpus <- tm_map(answer_corpus, removeNumbers)
+
+# 4. Removing punctuation
+clue_corpus <- tm_map(clue_corpus, removePunctuation)
+category_corpus <- tm_map(category_corpus, removePunctuation)
+answer_corpus <- tm_map(answer_corpus, removePunctuation)
+
+# 5. Removing stop words
+clue_corpus <- tm_map(clue_corpus, removeWords, stopwords("english"))
+category_corpus <- tm_map(category_corpus, removeWords, stopwords("english"))
+answer_corpus <- tm_map(answer_corpus, removeWords, stopwords("english"))
+
+DTM_clue <- DocumentTermMatrix(clue_corpus)
+DTM_category <- DocumentTermMatrix(category_corpus)
+DTM_answer <- DocumentTermMatrix(answer_corpus)
+
+#clue wordcloud - you may have to increase memory limit to run
+sums <- as.data.frame(colSums(as.matrix(DTM_clue)))
+sums <- rownames_to_column(sums)
+colnames(sums) <- c("term", "count")
+sums <- arrange(sums, desc(count))
+
+#what are top 10 words
+sums[1:10,]
+
+#make word cloud with top 50 words
+clue_50 <- sums[1:50,]
+
+wordcloud(words = clue_50$term, freq = clue_50$count,min.freq = 1,
+          max.words=100, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(6, "Blues"))
+
+#category wordcloud
+sums2 <- as.data.frame(colSums(as.matrix(DTM_category)))
+sums2 <- rownames_to_column(sums2)
+colnames(sums2) <- c("term", "count")
+sums2 <- arrange(sums2, desc(count))
+
+#what are top 10 words
+sums2[1:10,]
+
+#make word cloud with top 50 words
+cat_50 <- sums2[1:50,]
+
+wordcloud(words = cat_50$term, freq = cat_50$count,min.freq = 1,
+          max.words=100, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(6, "Blues"))
+
+
+#answer wordcloud
+sums3 <- as.data.frame(colSums(as.matrix(DTM_answer)))
+sums3 <- rownames_to_column(sums3)
+colnames(sums3) <- c("term", "count")
+sums3 <- arrange(sums3, desc(count))
+
+#what are top 10 words
+sums3[1:10,]
+
+#make word cloud with top 50 words
+ans_50 <- sums3[1:50,]
+
+wordcloud(words = ans_50$term, freq = ans_50$count,min.freq = 1,
+          max.words=100, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(6, "Blues"))
+
+# Add Accuracy to winners ----------
+winners <- winners %>%
+  mutate(accuracy = round(ansRight / (ansRight + ansWrong) *100))
+
+#who is in the 100% accuracy club
+onehundredclub <- filter(winners, accuracy == 100) %>%
+  select(firstname, lastname) %>%
+  distinct()
+
+#summarize career stats
+averages <- winners %>%
+  group_by(players_playerid, firstname, lastname) %>%
+  summarize(game_count = n(), avg_correct = mean(ansRight), avg_incorrect = mean(ansWrong),
+            total_correct = sum(ansRight), total_incorrect = sum(ansWrong),
+            max_score = max(finalscore),avg_acc = mean(accuracy), avg_score = mean(finalscore))
+
+ggplot(averages, aes(x = avg_correct, y = avg_incorrect, color = avg_score)) +
+  geom_point() +
+  xlab("Average Correct Answers") +
+  ylab("Average Incorrect Answers") +
+  ggtitle("Average Player Accuracy") +
+  theme(
+    axis.title.y = element_text(size = 12),
+    axis.text.y = element_text(size = 12),
+    axis.title.x = element_text(size = 12),
+    plot.title = element_text(size = 15)
+  )
 
 
 # Who Won Each Game -------------------------------------------------------
+champ <- winners %>% 
+  group_by(episode_gameid) %>% 
+  arrange(desc(finalscore)) %>% 
+  filter(row_number() == 1)
+
+#total games won
+games_won <- champ %>% 
+  group_by(players_playerid, firstname, lastname) %>%
+  summarise(games_won = n())
 
 
